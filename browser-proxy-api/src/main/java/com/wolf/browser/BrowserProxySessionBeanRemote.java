48bee7b453f76c8d56b8855bf7d0cfcebc2e732f
aladdin <aladdin@aladdin-OptiPlex-745>
2013-04-14 14:09:43 +0800

    实现通过browser proxy来刷新cookie的信息

--- browser-proxy-api/src/main/java/com/wolf/browser/BrowserProxySessionBeanRemote.java
+++ browser-proxy-api/src/main/java/com/wolf/browser/BrowserProxySessionBeanRemote.java
@@ -1,12 +1,20 @@
 package com.wolf.browser;
 
+import java.util.Map;
 import javax.ejb.Remote;
 
 /**
  *
- * @author Administrator
+ * @author aladdin
  */
 @Remote
 public interface BrowserProxySessionBeanRemote {
     
+    /**
+     * 获取最新cookie
+     * @param url
+     * @param cookieMap
+     * @return 
+     */
+    public Map<String, String> getNewCookie(String url, Map<String, String> cookieMap);
 }
--- browser-proxy-ejb/src/main/java/com/wolf/browser/BrowserProxySessionBean.java
+++ browser-proxy-ejb/src/main/java/com/wolf/browser/BrowserProxySessionBean.java
@@ -1,17 +1,43 @@
 package com.wolf.browser;
 
+import java.util.Map;
+import java.util.Set;
 import javax.ejb.Startup;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 /**
  *
- * @author Administrator
+ * @author aladdin
  */
 @Stateless
 @Startup
 @TransactionAttribute(TransactionAttributeType.NEVER)
 public class BrowserProxySessionBean implements BrowserProxySessionBeanRemote {
-    
+
+    @Override
+    public Map<String, String> getNewCookie(String url, Map<String, String> cookieMap) {
+        WebDriver driver = new InternetExplorerDriver();
+        //访问地址
+        driver.get(url);
+        //设置cookie
+        Cookie cookie;
+        Set<Map.Entry<String, String>> entrySet = cookieMap.entrySet();
+        for (Map.Entry<String, String> entry : entrySet) {
+            cookie = new Cookie(entry.getKey(), entry.getValue());
+            driver.manage().addCookie(cookie);
+        }
+        //重新访问地址
+        driver.get(url);
+        //获取新的cookie
+        Set<Cookie> allCookies = driver.manage().getCookies();
+        for (Cookie loadedCookie : allCookies) {
+            cookieMap.put(loadedCookie.getName(), loadedCookie.getValue());
+        }
+        return cookieMap;
+    }
 }
