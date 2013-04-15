5ac018d092911520c6214ca4f13853acc5c4df13
aladdin <aladdin@aladdin-OptiPlex-745>
2013-04-15 11:02:56 +0800

    增加远程调用工厂类，封装配置信息。

---
+++ browser-proxy-api/src/main/java/com/wolf/browser/BrowserProxySessionBeanRemoteFactory.java
@@ -0,0 +1,30 @@
+package com.wolf.browser;
+
+import java.util.Properties;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author aladdin
+ */
+public class BrowserProxySessionBeanRemoteFactory {
+
+    public static BrowserProxySessionBeanRemote getBrowserProxySessionBeanRemote() {
+        Properties props = new Properties();
+        props.setProperty("java.naming.factory.initial", "com.sun.enterprise.naming.SerialInitContextFactory");
+        props.setProperty("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
+        props.setProperty("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+        props.setProperty("org.omg.CORBA.ORBInitialHost", "192.168.64.50");
+        props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+        BrowserProxySessionBeanRemote remote;
+        try {
+            InitialContext ic = new InitialContext(props);
+            remote = (BrowserProxySessionBeanRemote) ic.lookup("com.wolf.browser.BrowserProxySessionBeanRemote");
+        } catch (NamingException e) {
+            System.err.println(e);
+            throw new RuntimeException("init BrowserProxySessionBeanRemote failure.");
+        }
+        return remote;
+    }
+}
